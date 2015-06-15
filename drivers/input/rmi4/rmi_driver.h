d47c9f728ce06d96d2f800a465446de53eb333e1
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2015-06-15 17:29:35 -0400

    Input - rmi4: embed the function modules in rmi_core
    the function modules can not be auto-loaded by udev. So at boot, the
    functions are not there and the touchpad does not answer.
    Force the module to be embedded in rmi_core so that when the touchpad
    is there, the functions are there too.
    
    There is not much use of having the functions separate anyway
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- drivers/input/rmi4/rmi_driver.h
+++ drivers/input/rmi4/rmi_driver.h
@@ -113,4 +113,20 @@ void rmi_unregister_physical_driver(void);
 int rmi_register_f01_handler(void);
 void rmi_unregister_f01_handler(void);
 
+#ifdef CONFIG_RMI4_F11
+int rmi_register_f11_handler(void);
+void rmi_unregister_f11_handler(void);
+#else
+static inline int rmi_register_f11_handler(void) {return 0;}
+static inline void rmi_unregister_f11_handler(void) {}
+#endif
+
+#ifdef CONFIG_RMI4_F30
+int rmi_register_f30_handler(void);
+void rmi_unregister_f30_handler(void);
+#else
+static inline int rmi_register_f30_handler(void) {return 0;}
+static inline void rmi_unregister_f30_handler(void) {}
+#endif
+
 #endif
