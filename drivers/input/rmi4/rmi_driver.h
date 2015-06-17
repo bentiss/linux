ec71c046809a4c7b063890d2e3794a2d9f3f6077
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2015-06-17 12:48:02 -0400

    Input - rmi4: set the unified input name
    The name of the device is now: "Synaptics PRODUCT_ID" to be able to
    differentiate the various models.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- drivers/input/rmi4/rmi_driver.h
+++ drivers/input/rmi4/rmi_driver.h
@@ -118,6 +118,7 @@ void rmi_unregister_physical_driver(void);
 
 int rmi_register_f01_handler(void);
 void rmi_unregister_f01_handler(void);
+char *rmi_f01_get_product_ID(struct rmi_function *fn);
 
 #ifdef CONFIG_RMI4_F11
 int rmi_register_f11_handler(void);
