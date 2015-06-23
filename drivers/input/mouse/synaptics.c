d6d6c83ca1da88cd924c306a0e98a7484653f65a
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2015-06-23 14:19:47 -0400

    Input: synaptics - handle rescan while using SMBus
    On resume, the PS/2 synaptics driver gets called with a rescan.
    This rescan switches back the touchpad in the PS/2 mode, and we
    need to re-enable the SMBus mode. Rely on the .resume() callback
    to notify the device that it needs to clear its state.
    
    Doing a rescan of the PS/2 port causes a segfault, due to the fact the
    i2c device gets unbound but never removed, meaning we try to call a
    resume function from a non-existant driver. Easiest fix is unregistering
    the device when we find the driver's been unbound, since this lets us
    bring the device back up on smbus and avoid segfaulting.
    
    Signed-off-by: Stephen Chandler Paul <cpaul@redhat.com>
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- drivers/input/mouse/synaptics.c
+++ drivers/input/mouse/synaptics.c
@@ -340,6 +340,7 @@ static int synaptics_setup_intertouch(struct psmouse *psmouse)
 {
 	int res;
 	struct synaptics_data *priv = psmouse->private;
+	struct device_driver *driver;
 
 	if (synaptics_intertouch == SYNAPTICS_INTERTOUCH_OFF)
 		return 0;
@@ -366,6 +367,26 @@ static int synaptics_setup_intertouch(struct psmouse *psmouse)
 	}
 
 	psmouse_reset(psmouse);
+
+	if (synaptics_smbus_client) {
+		if (synaptics_smbus_client->dev.driver) {
+			/*
+			 * The PS/2 port has been reset and the device is in an
+			 * unknown state. Send a resume command.
+			 */
+			driver = synaptics_smbus_client->dev.driver;
+			if (driver->resume)
+				driver->resume(&synaptics_smbus_client->dev);
+			return -1;
+		} else {
+			/*
+			 * The driver was unbound from the device but never
+			 * unregistered, unregister it ourselves before
+			 * registering it again
+			 */
+			i2c_unregister_device(synaptics_smbus_client);
+		}
+	}
 
 	/* Bind to already existing adapters right away */
 	i2c_for_each_dev(NULL, synaptics_attach_i2c_device);
