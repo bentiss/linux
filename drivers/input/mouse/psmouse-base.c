a49cf47ae19ff2d3936220735bf2b4ffd4a8605a
Lyude Paul <thatslyude@gmail.com>
2015-07-17 15:24:41 -0400

    Input: synaptics-rmi4 - add support for F03
    This adds basic functionality for PS/2 passthrough on Synaptics
    Touchpads using RMI4 through smbus.
    
    Reviewed-by: Andrew Duggan <aduggan@synaptics.com>
    Signed-off-by: Lyude Paul <thatslyude@gmail.com>
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    
    ---
    
    no changes in v3
    
    changes in v2:
    - switch to use rmi_dbg instead of plain dev_dbg

--- drivers/input/mouse/psmouse-base.c
+++ drivers/input/mouse/psmouse-base.c
@@ -1663,6 +1663,12 @@ static struct serio_device_id psmouse_serio_ids[] = {
 		.id	= SERIO_ANY,
 		.extra	= SERIO_ANY,
 	},
+	{
+		.type	= SERIO_RMI_PSTHRU,
+		.proto	= SERIO_ANY,
+		.id	= SERIO_ANY,
+		.extra	= SERIO_ANY,
+	},
 	{ 0 }
 };
 
